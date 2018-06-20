103438dc39a1bd86757d2d11e2c94a89eba6d0a1
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-06-20 20:42:39 +0300

    Added change the color of the coat, eyes and fireball by pressing

--- js/setup.js
+++ js/setup.js
@@ -8,6 +8,7 @@ var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var NUMBER_OF_WIZARDS = 4;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var setup = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -15,7 +16,17 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
+
 // var setupUserName = document.querySelector('.setup-user-name');
+
+var coatColor = setup.querySelector('.wizard-coat');
+var eyesColor = setup.querySelector('.wizard-eyes');
+var fireballColor = setup.querySelector('.setup-fireball');
+var inputCoatColor = setup.querySelector('input[name="coat-color"]');
+var inputEyesColor = setup.querySelector('input[name="eyes-color"]');
+var inputFireballColor = setup.querySelector('input[name="fireball-color"]');
+var setupWizardForm = document.querySelector('.setup-wizard-form');
+
 
 // Возвращает случайное целое число из диапозона min (включительно) и max (не включая max);
 var getRandomIntByRange = function (min, max) {
@@ -58,20 +69,50 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+// Убирает класс hidden у объекта setup и добавляет обработчик собития keydown на объект документ.
 var openPopup = function () {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
+  // stopPropagation на клавишу esc?
 };
 
+// Добаляет класс hidden объекту setup и убирает обработчик события keydown у объекта document.
 var closePopup = function () {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
-
+// Принимает событие event. Если нажата клавиша ESC, то запускает функцию closePopup.
 var onPopupEscPress = function (evt) {
+  // if (evt.keyCode === ESC_KEYCODE && !setupUserName.focused - поле поиска не в фокусе ) {
   if (evt.keyCode === ESC_KEYCODE) {
     closePopup();
+  }
+};
+
+// Принимает объект 1, массив, и объект 2.
+// Устанваливает объекту 1 цвет заливки случайным образом из массива arr и передаёт это значение в свойсвто value объекта 2.
+var toAssignColor = function (obj1, arr, obj2) {
+  obj1.style.fill = getRandomArrayItem(arr);
+  obj2.value = obj1.style.fill;
+};
+
+// Устанавливает цвет файрбола случайным образом из массива arr.
+var toAssignFireballColor = function (arr) {
+  fireballColor.style.backgroundColor = getRandomArrayItem(arr);
+  inputFireballColor.backgroundColor = fireballColor.style.backgroundColor;
+};
+
+// Обработчик событий клика на форме .Setup-Wizard-Form. Запускает функции из предложеных вариантов, в зависимости от того,
+// на каком элементе произошёл клик.
+var onSetupWizardFormClick = function (evt) {
+  switch (evt.target) {
+    case coatColor: toAssignColor(coatColor, COAT_COLORS, inputCoatColor);
+      break;
+    case eyesColor: toAssignColor(eyesColor, EYES_COLORS, inputEyesColor);
+      break;
+    case fireballColor: toAssignFireballColor(FIREBALL_COLORS);
+      break;
   }
 };
 
@@ -104,3 +145,5 @@ setupClose.addEventListener('keydown', function (evt) {
     closePopup();
   }
 });
+
+setupWizardForm.addEventListener('click', onSetupWizardFormClick);

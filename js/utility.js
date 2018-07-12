d08a13b5f85283e2dde40b22d79bde89023372a4
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-07-12 18:15:38 +0300

    Fixed travis test

--- js/utility.js
+++ js/utility.js
@@ -10,11 +10,11 @@
   // Возвращает случайный элемент массива. Выборка берётся из входящего массива arr.
   var getRandomArrayItem = function (arr) {
     return arr[getRandomIntByRange(0, arr.length)];
-  }
+  };
 
   window.utility = {
     COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+      'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
     setup: document.querySelector('.setup'),
     dialog: document.querySelector('.setup-user-pic'),

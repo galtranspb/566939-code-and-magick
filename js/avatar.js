1b44884a90dc55e45305a063a13395f4c2d2e88c
Anatoly <AnatoliySokolov1981@gmail.com>
2018-08-04 20:21:01 +0300

    Fixed avatar.js module

--- js/avatar.js
+++ js/avatar.js
@@ -5,6 +5,7 @@
 
   var fileChooser = document.querySelector('input[name=avatar]');
   var preview = document.querySelector('.setup-user-pic');
+  var setupOpenIcon = document.querySelector('.setup-open-icon');
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
@@ -19,6 +20,7 @@
 
       reader.addEventListener('load', function () {
         preview.src = reader.result;
+        setupOpenIcon.src = reader.result;
       });
 
       reader.readAsDataURL(file);
--- js/setup.js
+++ js/setup.js
@@ -8,7 +8,6 @@
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var setupUserName = setup.querySelector('.setup-user-name');
-  var preview = document.querySelector('.setup-user-pic');
 
   // Убирает класс hidden у объекта setup, добавляет обработчик собития keydown на объект документ и
   // устанавливает z-index 3.
@@ -27,7 +26,6 @@
     document.removeEventListener('keydown', onPopupEscPress);
     setup.style.top = '';
     setup.style.left = '';
-    preview.src = './img/user-1.jpg';
   };
 
   // Принимает событие event. Если нажата клавиша ESC и поле ввода имени не в фокусе,
